30e73afa65b79d717fcfec0b877294149921ceb2
Esteban2002Dev <tsuesteban2002@gmail.com>
2024-10-16 23:35:30 -0600

    Add PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { MainInterceptor } from './core/interceptor/main.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MainInterceptor,
-      multi: true
-  }
-  ]
+        provide: HTTP_INTERCEPTORS,
+        useClass: MainInterceptor,
+        multi: true
+    },
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
